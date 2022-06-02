579575d379bce9da3f6428651e750ca5af993ec4
Siljesandersen <silje_sandersen@hotmail.com>
2022-06-02 17:06:52 +0200

    feat: added api url

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Card, CardContainer, Page } from "./styles/LayoutStyles";
 
 function Projects() {
+  const [projects, setProjects] = useState();
+
+  useEffect(() => {}, []);
+
   return (
     <Page name="projects">
       <CardContainer>
---
+++ src/utils/api.js
@@ -0,0 +1,4 @@
+export const BASE_URL = "https://portfolio-api-siljes.herokuapp.com/";
+export const PROJECT_URL = `${BASE_URL}api/projects`;
+export const CONTACT_URL = `${BASE_URL}api/contacts`;
+export const POPULATE = "?populate=*";
